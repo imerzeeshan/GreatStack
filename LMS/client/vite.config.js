f07aa97d4881faa3105d7b5bc85f0ed435f5b46c
Md Zeeshan <imerzeeshan@gmail.com>
2025-04-21 12:52:25 +0530

    LMS Web Application

--- LMS/client/vite.config.js
+++ LMS/client/vite.config.js
@@ -1,9 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   css: {
-    postcss: './postcss.config.js'
-  }
-})
+    postcss: {
+      fontSize: {
+        "course-details-heading-small": ["26px", "36px"],
+        "course-details-heading-large": ["26px", "36px"],
+        "home-heading-large": ["48px", "56px"],
+        "home-heading-small": ["28px", "34px"],
+      },
+    },
+  },
+});
