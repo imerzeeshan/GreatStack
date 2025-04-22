d66a3847807e23e265052dc50ab95129d1e87d1d
Md Zeeshan <imerzeeshan@gmail.com>
2025-04-22 11:10:59 +0530

    LMS Web Application

---
+++ LMS/frontend/src/context/AppContext.jsx
@@ -0,0 +1,23 @@
+import { createContext, useEffect, useState } from "react";
+import { dummyCourses } from "../assets/assets";
+
+export const AppContext = createContext();
+
+export const AppContextProvider = (props) => {
+  const currency = import.meta.env.VITE_CURRENCY;
+  const [allCourses, setAllCourses] = useState([]);
+
+  //Fetch All Courses
+  const fetchAllCourses = async () => {
+    await setAllCourses(dummyCourses);
+  };
+
+  useEffect(() => {
+    fetchAllCourses();
+  }, []);
+
+  const value = { currency, allCourses };
+  return (
+    <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
+  );
+};
