7f14c99f3704563ee8ea274061f0baa0185067e3
Md Zeeshan <imerzeeshan@gmail.com>
2025-05-04 22:21:52 +0530

    Creating Backend

--- LMS/server/server.js
+++ LMS/server/server.js
@@ -2,18 +2,20 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import connectDB from "./configs/mongodb.js";
+import { clerkWebhooks } from "./controllers/webhooks.js";
 
 // Initialize Express
 const app = express();
 
 // Connect to database
-await connectDB()
+await connectDB();
 
 // Middlewares
 app.use(cors());
 
 // Routes
 app.get("/", (req, res) => res.send("API Working"));
+app.post("/clerk", express.json(), clerkWebhooks);
 
 // Port
 const PORT = process.env.PORT || 5000;
